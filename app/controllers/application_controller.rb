5ce3ea8c947fd7848354ee1987635969c9f35fe2
Donny Roufs <iamchets@yahoo.com>
2020-02-20 19:05:15 +0100

    finished getting-started guide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    http_basic_authenticate_with name: "iamchets", password: "root", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   # Creates a REST resource, which basically means all CRUD enpoints.
-  resources :articles 
+  # resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 end
